c11e8c468baf5fd767bc1e5b2e7464b565b80f40
Khoi Nguyen <63745633+KhoiUna@users.noreply.github.com>
2020-06-17 19:34:15 -0500

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -1,3 +1,5 @@
+//This sound is for white keys
+
 const fSound = new Audio("sound/F.m4a");
 const fNote = {
   play() {
@@ -9,3 +11,163 @@ const fNote = {
     fSound.pause();
   },
 };
+/////
+
+/////
+const gSound = new Audio("sound/G.m4a");
+const gNote = {
+  play() {
+    gSound.load();
+    gSound.play();
+    gSound.volume = 1;
+  },
+  pause() {
+    gSound.pause();
+  },
+};
+/////
+
+/////
+const aSound = new Audio("sound/A.m4a");
+const aNote = {
+  play() {
+    aSound.load();
+    aSound.play();
+    aSound.volume = 1;
+  },
+  pause() {
+    aSound.pause();
+  },
+};
+/////
+
+/////
+const bSound = new Audio("sound/B.m4a");
+const bNote = {
+  play() {
+    bSound.load();
+    bSound.play();
+    bSound.volume = 1;
+  },
+  pause() {
+    bSound.pause();
+  },
+};
+/////
+
+/////
+const cSound = new Audio("sound/C.m4a");
+const cNote = {
+  play() {
+    cSound.load();
+    cSound.play();
+    cSound.volume = 1;
+  },
+  pause() {
+    cSound.pause();
+  },
+};
+/////
+
+/////
+const dSound = new Audio("sound/D.m4a");
+const dNote = {
+  play() {
+    dSound.load();
+    dSound.play();
+    dSound.volume = 1;
+  },
+  pause() {
+    dSound.pause();
+  },
+};
+/////
+
+/////
+const eSound = new Audio("sound/E.m4a");
+const eNote = {
+  play() {
+    eSound.load();
+    eSound.play();
+    eSound.volume = 1;
+  },
+  pause() {
+    eSound.pause();
+  },
+};
+/////
+
+/////
+const fHighSound = new Audio("sound/Fhigh.m4a");
+const fHighNote = {
+  play() {
+    fHighSound.load();
+    fHighSound.play();
+    fHighSound.volume = 1;
+  },
+  pause() {
+    fHighSound.pause();
+  },
+};
+/////
+
+/////
+const gHighSound = new Audio("sound/Ghigh.m4a");
+const gHighNote = {
+  play() {
+    gHighSound.load();
+    gHighSound.play();
+    gHighSound.volume = 1;
+  },
+  pause() {
+    gHighSound.pause();
+  },
+};
+/////
+
+/////
+const aHighSound = new Audio("sound/Ahigh.m4a");
+const aHighNote = {
+  play() {
+    aHighSound.load();
+    aHighSound.play();
+    aHighSound.volume = 1;
+  },
+  pause() {
+    aHighSound.pause();
+  },
+};
+/////
+
+/////
+const bHighSound = new Audio("sound/Bhigh.m4a");
+const bHighNote = {
+  play() {
+    bHighSound.load();
+    bHighSound.play();
+    bHighSound.volume = 1;
+  },
+  pause() {
+    bHighSound.pause();
+  },
+};
+
+/////
+
+/////
+
+/////
+/////
+
+/////
+/////
+
+/////
+/////
+
+/////
+/////
+
+/////
+
+//This sound is for black keys
