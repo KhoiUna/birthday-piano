377eb1dfee8d13a2bc20510333a57fea1261c920
KhoiNguyen <thorwaitson@gmail.com>
2022-08-04 23:37:08 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "./",
   build: {
     manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        play: resolve(__dirname, "play.html"),
+      },
+    },
   },
 });
