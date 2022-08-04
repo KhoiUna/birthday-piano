5136fc8db1e14b71912c11822283fd1bdaddff6f
KhoiNguyen <thorwaitson@gmail.com>
2022-08-04 14:16:03 +0700

    add js files

---
+++ js/global.js
@@ -0,0 +1,1 @@
+export const red = "rgb(240, 80, 80)";
---
+++ js/initialState.js
@@ -0,0 +1,21 @@
+import { red } from "./global";
+
+// Set initial state
+// Slide One
+document.querySelector(".lyric-grid-one .lyric-box-one").style.backgroundColor =
+  red;
+document.getElementById("g").style.backgroundColor = red;
+
+// Slide Two
+document.querySelector(".lyric-grid-two .lyric-box-one").style.backgroundColor =
+  red;
+
+// Slide Three
+document.querySelector(
+  ".lyric-grid-three .lyric-box-one"
+).style.backgroundColor = red;
+
+// Slide Four
+document.querySelector(
+  ".lyric-grid-four .lyric-box-one"
+).style.backgroundColor = red;
