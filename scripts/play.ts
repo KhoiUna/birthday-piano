df810bb6fd52266c9c8445a01a616628229440a9
KhoiNguyen <thorwaitson@gmail.com>
2022-08-04 22:14:10 +0700

    update play.ts

--- scripts/play.ts
+++ scripts/play.ts
@@ -1,5 +1,7 @@
 import { trackerFunction } from "./tracker";
 import PianoKeyboard from "./utils/PianoKeyboard";
+import happy_birthday from "../songs/happy_birthday.json";
+import { red } from "./global";
 
 let tracker = 1;
 
@@ -7,10 +9,23 @@ document.querySelectorAll(".piano > *").forEach((element) => {
   element.addEventListener("pointerdown", (event) => {
     PianoKeyboard.press(event);
 
+    const target = event.target as HTMLDivElement;
+    const key = target.id;
+
+    if (!happy_birthday.pianoMap[key]?.includes(tracker)) return;
+
     if (trackerFunction(tracker)) tracker++;
   });
 
   element.addEventListener("pointerup", (event) => {
     PianoKeyboard.release(event);
+
+    const target = event.target as HTMLDivElement;
+    const key = target.id;
+
+    if (happy_birthday.pianoMap[key]?.includes(tracker)) {
+      target.style.backgroundColor = red;
+      return;
+    }
   });
 });
