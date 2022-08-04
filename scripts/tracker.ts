309ac7039658e7b0152ff4e398e8bed479477304
KhoiNguyen <thorwaitson@gmail.com>
2022-08-04 16:58:29 +0700

    update track.ts

--- scripts/tracker.ts
+++ scripts/tracker.ts
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import { lightYellow, red } from "./global";
 import Quote from "./utils/Quote";
+import blowImage from "../img/blow.jpg";
 
 // This function tracks the progress of the user
 export const trackerFunction = (tracker: number) => {
@@ -376,7 +377,7 @@ export const trackerFunction = (tracker: number) => {
     document.querySelector<HTMLDivElement>("footer").style.padding = "3%";
 
     // Change avatar to Cheering
-    document.querySelector<HTMLImageElement>("#avatar").src = "./img/blow.jpg";
+    document.querySelector<HTMLImageElement>("#avatar").src = blowImage;
 
     // Play Cheering sound
     const cheering = new Audio("sound/cheering.mp3");
