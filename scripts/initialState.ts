039adb103ff596517cb37f7a82cdb580bae53fbd
KhoiNguyen <thorwaitson@gmail.com>
2022-08-08 00:45:05 +0700

    update initialState.ts to load wish from server

--- scripts/initialState.ts
+++ scripts/initialState.ts
@@ -1,7 +1,15 @@
 import { red } from "./global";
+import { parseParam } from "./helpers/parseParam";
 import { randomTheme } from "./helpers/randomTheme";
+import Wish from "./utils/Wish";
 
-// Set initial states
+// Declare elements
+const mainElement = document.querySelector("main");
+mainElement.style.display = "none";
+
+const loaderElement = document.querySelector("#loader");
+
+// Set initial states of
 // 1st slide
 document.querySelector<HTMLDivElement>(
   ".lyric-grid-one .lyric-box-one"
@@ -25,3 +33,89 @@ document.querySelector<HTMLDivElement>(
 
 // Random background color
 randomTheme();
+
+// Function to load sound
+const loadSound = () => {
+  // Register piano sounds to createjs.Sound.
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/F.m4a", "f");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/G.m4a", "g");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/A.m4a", "a");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/B.m4a", "b");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/C.m4a", "c");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/D.m4a", "d");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/E.m4a", "e");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Fhigh.m4a", "f-high");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Ghigh.m4a", "g-high");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Ahigh.m4a", "a-high");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Bhigh.m4a", "b-high");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Fsharp.m4a", "black-key-one");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Gsharp.m4a", "black-key-two");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Asharp.m4a", "black-key-three");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Csharp.m4a", "black-key-four");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Dsharp.m4a", "black-key-five");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Fsharphigh.m4a", "black-key-six");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Gsharphigh.m4a", "black-key-seven");
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/Asharphigh.m4a", "black-key-eight");
+
+  // Register cheering sound to createjs.Sound.
+  // @ts-ignore
+  createjs.Sound.registerSound("/sound/cheering.mp3", "cheering");
+};
+
+// Set initial states of wishText
+export const wish = { wishText: "", fromUser: "" };
+
+window.addEventListener("load", async () => {
+  loadSound();
+
+  const idParam = Number(parseParam("id"));
+  if (idParam === 0) return;
+
+  // Load wish and set initial states of wish
+  const response = await Wish.fetchWish(idParam);
+
+  if (response.error) {
+    loaderElement.remove();
+    mainElement.style.display = "block";
+    mainElement.innerHTML = `<h1 style="margin: 2rem">${response.error}</h1>`;
+    return;
+  }
+
+  loaderElement.remove();
+  mainElement.style.display = "block";
+  const { date_of_birth, from_user, image_url, to_user, wish_text } =
+    response.success;
+
+  wish.fromUser = from_user;
+  wish.wishText = wish_text;
+
+  document.querySelector<HTMLSpanElement>("#from-user").innerText = from_user;
+  document.querySelector<HTMLSpanElement>("#to-user").innerText =
+    ", " + to_user;
+
+  // TODO if not yet birthday, show message
+  //
+
+  document.querySelector<HTMLImageElement>("#avatar").src = image_url;
+  document.querySelector<HTMLImageElement>("#avatar").alt =
+    from_user + "'s avatar";
+});
