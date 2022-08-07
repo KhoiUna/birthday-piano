7ddd8ec6164aa744f4e230e972932bce19364e50
KhoiNguyen <thorwaitson@gmail.com>
2022-08-07 11:18:44 +0700

    update: play sound faster with createjs-soundjs

--- scripts/play.ts
+++ scripts/play.ts
@@ -3,29 +3,6 @@ import PianoKeyboard from "./utils/PianoKeyboard";
 import happy_birthday from "../songs/happy_birthday.json";
 import { red } from "./global";
 
-// Load all audios
-export const audioMap = {
-  f: new Audio("/sound/F.m4a"),
-  g: new Audio("/sound/G.m4a"),
-  a: new Audio("/sound/A.m4a"),
-  b: new Audio("/sound/B.m4a"),
-  c: new Audio("/sound/C.m4a"),
-  d: new Audio("/sound/D.m4a"),
-  e: new Audio("/sound/E.m4a"),
-  "f-high": new Audio("/sound/Fhigh.m4a"),
-  "g-high": new Audio("/sound/Ghigh.m4a"),
-  "a-high": new Audio("/sound/Ahigh.m4a"),
-  "b-high": new Audio("/sound/Bhigh.m4a"),
-  "black-key-one": new Audio("/sound/Fsharp.m4a"),
-  "black-key-two": new Audio("/sound/Gsharp.m4a"),
-  "black-key-three": new Audio("/sound/Asharp.m4a"),
-  "black-key-four": new Audio("/sound/Csharp.m4a"),
-  "black-key-five": new Audio("/sound/Dsharp.m4a"),
-  "black-key-six": new Audio("/sound/Fsharphigh.m4a"),
-  "black-key-seven": new Audio("/sound/Gsharphigh.m4a"),
-  "black-key-eight": new Audio("/sound/Bhigh.m4a"),
-};
-
 let tracker = 1;
 
 document.querySelectorAll(".piano > *").forEach((element) => {
--- scripts/utils/PianoKeyboard.ts
+++ scripts/utils/PianoKeyboard.ts
@@ -1,18 +1,32 @@
-import { audioMap } from "../play";
+// Register piano sounds to createjs.Sound.
+// @ts-nocheck
+createjs.Sound.registerSound("/sound/F.m4a", "f");
+createjs.Sound.registerSound("/sound/G.m4a", "g");
+createjs.Sound.registerSound("/sound/A.m4a", "a");
+createjs.Sound.registerSound("/sound/B.m4a", "b");
+createjs.Sound.registerSound("/sound/C.m4a", "c");
+createjs.Sound.registerSound("/sound/D.m4a", "d");
+createjs.Sound.registerSound("/sound/E.m4a", "e");
+createjs.Sound.registerSound("/sound/Fhigh.m4a", "f-high");
+createjs.Sound.registerSound("/sound/Ghigh.m4a", "g-high");
+createjs.Sound.registerSound("/sound/Ahigh.m4a", "a-high");
+createjs.Sound.registerSound("/sound/Bhigh.m4a", "b-high");
+createjs.Sound.registerSound("/sound/Fsharp.m4a", "black-key-one");
+createjs.Sound.registerSound("/sound/Gsharp.m4a", "black-key-two");
+createjs.Sound.registerSound("/sound/Asharp.m4a", "black-key-three");
+createjs.Sound.registerSound("/sound/Csharp.m4a", "black-key-four");
+createjs.Sound.registerSound("/sound/Dsharp.m4a", "black-key-five");
+createjs.Sound.registerSound("/sound/Fsharphigh.m4a", "black-key-six");
+createjs.Sound.registerSound("/sound/Gsharphigh.m4a", "black-key-seven");
+createjs.Sound.registerSound("/sound/Asharphigh.m4a", "black-key-eight");
 
 export default class PianoKeyboard {
-  static currentAudio = new Audio();
-
   static play(event: Event) {
     const target = event.target as HTMLDivElement;
     const key = target.id;
 
-    const audioFile = audioMap[key];
-
-    this.currentAudio = audioFile;
-    this.currentAudio.load();
-    this.currentAudio.play();
-    this.currentAudio.volume = 1;
+    const sound = createjs.Sound.play(key); // play using id
+    sound.volume = 0.5;
   }
 
   //Change keyboard's color functions
@@ -30,8 +44,6 @@ export default class PianoKeyboard {
   }
 
   static release(event: Event) {
-    this.currentAudio.pause(); // stop the previous sound
-
     const target = event.target as HTMLDivElement;
     const key = target.id;
     // If the key is one of the black keys, becasue black keys have longer id names
