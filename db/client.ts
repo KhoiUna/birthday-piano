fa51fd6af0277fa0856b06f907d2589794b0c483
KhoiNguyen <thorwaitson@gmail.com>
2022-08-07 15:31:00 +0700

    add postgres db

---
+++ db/client.ts
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URI,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
+});
+client
+  .connect()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.error("Error connecting to database", error);
+  });
+
+export default client;
