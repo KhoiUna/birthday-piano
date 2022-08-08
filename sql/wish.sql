ce70fa683e12cd00ab443a415c592b177ae0d9b7
KhoiNguyen <thorwaitson@gmail.com>
2022-08-08 09:26:27 +0700

    update wish.sql

--- sql/wish.sql
+++ sql/wish.sql
@@ -1,11 +1,15 @@
+-- Create uuid extension for Postgres: https://kb.objectrocket.com/postgresql/uuid-generator-in-postgresql-775
+CREATE extension IF NOT EXISTS "uuid-ossp";
+
 -- PostgreSQL
 CREATE TABLE IF NOT EXISTS wish (
-    id SERIAL PRIMARY KEY,
+    id uuid DEFAULT uuid_generate_v4(),
     from_user VARCHAR(45) NOT NULL,
     to_user VARCHAR(45) NOT NULL,
     email VARCHAR(255) NOT NULL,
     date_of_birth TIMESTAMP NOT NULL,
     wish_text VARCHAR(100) NOT NULL,
     image_url VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
