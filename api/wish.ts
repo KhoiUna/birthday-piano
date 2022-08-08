d6df53cc5eb8d00d99abc0ec2c6cbc32b97e7562
KhoiNguyen <thorwaitson@gmail.com>
2022-08-08 10:16:10 +0700

    update wish.ts api to validate uuid

--- api/wish.ts
+++ api/wish.ts
@@ -1,8 +1,16 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import client from "../db/client";
+import { validate } from "uuid";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   if (req.method === "GET") {
+    if (!validate(req.query.id as string)) {
+      return res.status(200).json({
+        success: false,
+        error: "No birthday wish found",
+      });
+    }
+
     const sql =
       "SELECT from_user, to_user, date_of_birth, image_url, wish_text FROM wish WHERE id = $1;";
     const { rows } = await client.query(sql, [req.query.id]);
