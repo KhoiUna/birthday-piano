31c9e1a9d5d8e9071df71df47bd1da8fb48b1d3c
KhoiUna <thorwaitson@gmail.com>
2024-03-12 00:24:04 -0500

    modified:   db/client.ts

--- db/client.ts
+++ db/client.ts
@@ -1,21 +1,21 @@
-import { Client } from "pg";
+import pg from "pg";
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URI,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? {
-          rejectUnauthorized: false,
-        }
-      : false,
+const client = new pg.Client({
+	connectionString: process.env.DATABASE_URI,
+	ssl:
+		process.env.NODE_ENV === "production"
+			? {
+					rejectUnauthorized: false,
+			  }
+			: false,
 });
 client
-  .connect()
-  .then(() => {
-    console.log("Database connected");
-  })
-  .catch((error) => {
-    console.error("Error connecting to database", error);
-  });
+	.connect()
+	.then(() => {
+		console.log("Database connected");
+	})
+	.catch((error) => {
+		console.error("Error connecting to database", error);
+	});
 
 export default client;
